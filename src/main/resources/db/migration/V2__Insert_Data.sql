8550203ea7fd4445d64852ae83b208cfac161805
TemirJohn <timur.dzhuldasov@outlook.com>
2025-12-16 21:17:27 +0500

    add buy func

--- src/main/resources/db/migration/V2__Insert_Data.sql
+++ src/main/resources/db/migration/V2__Insert_Data.sql
@@ -1,19 +1,21 @@
 INSERT INTO category (id, name) VALUES (1, 'RPG');
 INSERT INTO category (id, name) VALUES (2, 'Shooter');
 INSERT INTO category (id, name) VALUES (3, 'Strategy');
+SELECT setval('category_id_seq', (SELECT MAX(id) FROM category));
 
 INSERT INTO permissions (id, name) VALUES (1, 'ROLE_USER');
 INSERT INTO permissions (id, name) VALUES (2, 'ROLE_ADMIN');
+SELECT setval('permissions_id_seq', (SELECT MAX(id) FROM permissions));
 
 INSERT INTO users (id, username, email, password)
-VALUES (1, 'admin', 'admin@game.kz', 'admin123'); -- Для продакшена пароль надо хешировать!
+VALUES (1, 'admin', 'admin@game.kz', 'admin123');
 
 INSERT INTO users (id, username, email, password)
 VALUES (2, 'gamer_pro', 'gamer@game.kz', 'gamer123');
+SELECT setval('users_id_seq', (SELECT MAX(id) FROM users));
 
-
-INSERT INTO user_permissions (user_id, permission_id) VALUES (1, 2); -- admin is ADMIN
-INSERT INTO user_permissions (user_id, permission_id) VALUES (2, 1); -- gamer is USER
+INSERT INTO user_permissions (user_id, permission_id) VALUES (1, 2);
+INSERT INTO user_permissions (user_id, permission_id) VALUES (2, 1);
 
 INSERT INTO game (id, title, description, price, image_url, category_id)
 VALUES (1, 'The Witcher 3', 'Geralt of Rivia hunting monsters', 29.99, 'https://example.com/witcher.jpg', 1);
